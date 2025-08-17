bb452d992d28afaca7f6516e70c63f2cb1444272
Kalpesh <kalpeshchabukswar@gmail.com>
2025-08-17 16:35:32 +0530

    Update settings.py

--- foodmood_backend/foodmood_backend/settings.py
+++ foodmood_backend/foodmood_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-gcp_427n5-5*ph&-w=jieytg04dv1h$$2%21-(kbtu8*)p3_w-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
