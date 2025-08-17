97fa4744c9cb366e7e3fc726a624145c0a1a59d2
KalpeshC28 <kalpeshchabukswar@gmail.com>
2025-08-18 01:07:22 +0530

    UPDATE: Update commit message and files

--- foodmood_backend/recipes/views.py
+++ foodmood_backend/recipes/views.py
@@ -4,23 +4,11 @@ from rest_framework import status
 
 class FusionRecipeView(APIView):
     def post(self, request):
-        # Extract data from request
         cuisine1 = request.data.get('cuisine1')
         cuisine2 = request.data.get('cuisine2')
         servings = request.data.get('servings')
         mealType = request.data.get('mealType')
 
-
-
-    query = f"{cuisine_1} {cuisine_2} {meal_type} fusion"
-    url = f"https://api.spoonacular.com/recipes/complexSearch"
-    params = {
-        "query": query,
-        "number": 5,
-        "apiKey": config("SPOONACULAR_API_KEY")
-    }
-
-    response = requests.get(url, params=params)
-   return Response({
+        return Response({
             "message": f"Fusion of {cuisine1} and {cuisine2} for {servings} servings of {mealType} coming right up!"
         }, status=status.HTTP_200_OK)
